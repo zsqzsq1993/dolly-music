0b8e13b8428f0f4981c298f3b0ad914de5ef4cb8
zsqzsq1993 <zsqzsq1993@yeah.net>
2020-08-12 23:09:09 +0800

    init suggest component, can search according to query

--- src/components/m-search/MySearch.vue
+++ src/components/m-search/MySearch.vue
@@ -13,13 +13,13 @@
       </ul>
     </div>
     <div class="suggest-wrapper" v-show="query">
-      <suggest></suggest>
+      <suggest ref="suggest"></suggest>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  import {Vue, Component} from 'vue-property-decorator'
+  import {Vue, Component, Watch} from 'vue-property-decorator'
   import {getHotSearch} from 'src/api/getSearch'
   import SearchBox from 'base/m-search-box/SearchBox.vue'
   import Suggest from 'components/m-suggest/Suggest.vue'
@@ -36,6 +36,11 @@
     }
   })
   export default class extends Vue {
+    @Watch('query')
+    dispatchRequest(newVal: string) {
+     (this.$refs.suggest as any).query(newVal, 1, true)
+    }
+
     hots: Array<HotKey> = []
     query = ''
 
--- src/components/m-suggest/Suggest.vue
+++ src/components/m-suggest/Suggest.vue
@@ -1,14 +1,67 @@
 <template>
-    <div class="">
-
+    <div class="m-suggest">
+      <ul>
+        <li v-for="(item, idx) in queryResults"
+            :key="idx">
+          <i :class="getIconCls(item)"></i>
+          <p class="content" v-text="getText(item)"></p>
+        </li>
+      </ul>
     </div>
 </template>
 
 <script lang="ts">
-    import {Prop, Component, Vue} from 'vue-property-decorator'
+    import {Component, Vue} from 'vue-property-decorator'
+    import {getSearch} from 'src/api/getSearch'
+    import {createSong, isValidSong, Song, SongConfig} from 'src/assets/ts/Song'
+
+    const TYPE_SINGER = 'singer'
+
+    interface Singer {
+      type: string;
+      singername: string;
+    }
 
     @Component
     export default class extends Vue {
+      queryResults: Array<any> = []
+
+      query(keyword: string, page: number, zhida: boolean) {
+        getSearch(keyword, page, zhida).then((response: any) => {
+          console.log(response)
+          const suggestion: Array<Singer | Song> = []
+          if (response.code === 0) {
+            response = response.data
+            if (response.zhida && response.zhida.singerid && page === 1) {
+              suggestion.push({...response.zhida, ...{type: TYPE_SINGER}} as Singer)
+            }
+
+            if (response.song && response.song.list && response.song.list.length) {
+              response.song.list.forEach((item: any) => {
+                if (isValidSong(item)) {
+                  suggestion.push(createSong(item as SongConfig))
+                }
+              })
+            }
+
+            this.queryResults = suggestion
+          } else {
+            throw new Error(`can not get search results by keyword ${keyword}`)
+          }
+        })
+      }
+
+      getIconCls(item: Singer | Song) {
+        return (item as Singer).type === TYPE_SINGER
+          ? 'icon-mine'
+          : 'icon-music'
+      }
+
+      getText(item: Singer | Song) {
+        return (item as Singer).type === TYPE_SINGER
+          ? (item as Singer).singername
+          : (item as Song).songname + '-' + (item as Song).singer
+      }
     }
 </script>
 
